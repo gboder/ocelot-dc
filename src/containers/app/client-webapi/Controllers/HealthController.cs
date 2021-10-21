1ae2db777c1f3bc671bfe2785271c70d8fe9223a
Gautier Boder <gboder@outlook.com>
2021-10-21 22:33:15 +0200

    added health probe to webapi

---
+++ src/containers/app/client-webapi/Controllers/HealthController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+
+namespace client_webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public bool IsFailing => _memoryCache.Get<bool>(nameof(HealthController));
+
+        public HealthController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
+        }
+
+        [HttpGet]
+        public IActionResult Get() => IsFailing ? StatusCode(500) : Ok();
+
+        [HttpPost("set/{failing}")]
+        public IActionResult Set(bool failing)
+        {
+            _memoryCache.Set<bool>(nameof(HealthController), failing);
+            return Ok(IsFailing);
+        }
+    }
+}
--- src/containers/app/client-webapi/Startup.cs
+++ src/containers/app/client-webapi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Consul.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,12 +27,13 @@ namespace client_webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMemoryCache();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "client_webapi", Version = "v1" });
             });
+            services.AddConsul();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
